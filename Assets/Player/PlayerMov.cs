ab12eeb92c975ca0ff828b50babe48ef2fa543f6
Da Silva <Da Silva@DESKTOP-MDMAS8V>
2025-08-01 15:30:17 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Player/PlayerMov.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float speed = 1;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Vector2 mov;
+    [SerializeField] Animator anim;
+
+    void Update()
+    {
+        mov.x = Input.GetAxis("Horizontal");
+        mov.y = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Horizontal", mov.x);
+        anim.SetFloat("Vertical", mov.y);
+        anim.SetFloat("Speed", mov.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + mov.normalized * speed * Time.fixedDeltaTime);
+    }
+}
